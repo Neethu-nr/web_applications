5752f3ee69168dd4a9993bd363d48b3a9cb2861a
neethu renjith <neethurenjith16@gmail.com>
2020-07-01 15:11:16 -0700

    calendar done

--- project 3/DatePicker.js
+++ project 3/DatePicker.js
@@ -6,9 +6,11 @@ function getNumDays(month){
     return (month in [0,2,4,6,7,9,11])? 31 :30;
 }
 function range(start, end) {
+    // returns array of continues numbers in range
     return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
 }
 function generateGrid(id,startDay, days, fixedDate){
+    // generates grid in html
     var gridTemplate = ``;
     for(var i=0; i< (days.length/7); i++){
         gridTemplate += `<tr >`;
@@ -34,13 +36,13 @@ function generateGrid(id,startDay, days, fixedDate){
         gridTemplate += `</tr>`;
     }
 
-    var Months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     var grid = `<table class="calendar" id="${id}Calendar"$>
                     <tr>
                         <th colspan="7" >
                         <div class="button-left button-year-left"></div>
                         <div class="button-left button-month-left"></div> 
-                        <span class="year" month=${Months[fixedDate.month]} year=${fixedDate.year}></span>
+                        <span class="year" month=${months[fixedDate.month]} year=${fixedDate.year}></span>
                         <div class="button-right button-month-right"></div>
                         <div class="button-right button-year-right"></div>
                         </th>
@@ -60,13 +62,17 @@ function generateGrid(id,startDay, days, fixedDate){
     
 }
 
+// DatePicker class displays clickable calendar widget
 function DatePicker(id, callback){
-    // valid date selected - invoke callback
-    function callbackWrapper(cell, table, event){
-        // get date
-        var numToMonths = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12};
+    var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var monthToNum = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12};
+    var element = document.getElementById(id);
+    var table;
+
+    function callbackWrapper(cell, event){
+        // when cells of table are clicked, highlights correct cell and calls callback
         var row = table.rows[0].cells[0].getElementsByTagName("span")[0];  
-        var clickedDate = {'month':numToMonths[row.getAttribute("month")],
+        var clickedDate = {'month':monthToNum[row.getAttribute("month")],
                         'day':cell.innerHTML,
                         'year':row.getAttribute("year")
                         }; 
@@ -78,8 +84,9 @@ function DatePicker(id, callback){
         event.stopPropagation();
     }   
 
-    DatePicker.prototype.render = function(date){
-        // display calendar
+
+    function displayDate(date){
+        // display calendar month with given date
         var fixedDate = {'month':date.getMonth(),
                         'day':date.getDate(),
                         'year':date.getFullYear()
@@ -94,22 +101,67 @@ function DatePicker(id, callback){
                     ];
 
         // convert to grid display
-        var element = document.getElementById(id);
         element.innerHTML = generateGrid(id,startDay, days, fixedDate);
-        
-        // callback on click - event listener added per cell
-        var calendarElement = document.getElementById(`${id}Calendar`);
-        for (var i=2; i<calendarElement.rows.length; i++){
-            for(var j=0; j<calendarElement.rows[i].cells.length; j++){
-                var cell = calendarElement.rows[i].cells[j];
+        table = document.getElementById(`${id}Calendar`);
+    }
+
+    function addListeners(){
+        // Makes the calendar functional
+        // Event listener added per cell
+        for (var i=2; i<table.rows.length; i++){
+            for(var j=0; j<table.rows[i].cells.length; j++){
+                var cell = table.rows[i].cells[j];
                 if (cell.classList.contains("curMonth")){
                     cell.onclick=function(){
-                            callbackWrapper(this, calendarElement, event)
+                            callbackWrapper(this, event)
                         };            
                 }
             }
         }
-    };
 
+        // buttons to change year and month
+        element.getElementsByClassName("button-year-left")[0].onclick = function(){
+            updateYear(-1,event)};
+        element.getElementsByClassName("button-year-right")[0].onclick = function(){
+            updateYear(1,event)};
+        element.getElementsByClassName("button-month-left")[0].onclick = function(){
+            updateMonth(-1,event)};
+        element.getElementsByClassName("button-month-right")[0].onclick = function(){
+            updateMonth(1,event)};    
+    }
+
+    function updateMonth(delta, event){
+        // changes month on calendar
+        var row = table.getElementsByClassName("year")[0];
+        var month = delta + monthToNum[row.getAttribute("month")]-1;
+        month = (month + 12) %12 + 1;
+        row.setAttribute("month", months[month-1]);
+        
+        // update days
+        var day = element.getElementsByClassName("selected")[0].innerHTML
+        var year = row.getAttribute("year");
+        displayDate(new Date(`${month}/${day}/${year}`));
+        addListeners();
+        event.stopPropagation();
+    }
+
+    function updateYear(delta, event){
+        // changes year on calendar
+        var row =table.getElementsByClassName("year")[0];
+        var year = delta + Number(row.getAttribute("year"));
+        row.setAttribute("year", year); 
+
+        // update days
+        var day = element.getElementsByClassName("selected")[0].innerHTML
+        var month = row.getAttribute("month");
+        displayDate(new Date(`${month}/${day}/${year}`));
+        addListeners();
+        event.stopPropagation();
+    }
+
+    DatePicker.prototype.render = function(date){
+        displayDate(date);
+        addListeners();
+    };
 }
 
